a45e68fab98aaf57bcb4f2b1f033b347f1e621a4
Will Cheng <willchengsters@gmail.com>
2018-03-21 10:50:24 -0700

    turning app into class component

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import './app.css';
-import logo from '../assets/images/logo.svg';
-import reactPumpkin from '../assets/images/october-react-logo-glow.png';
-import webs from '../assets/images/web-corner.png';
-import spider from '../assets/images/spider.png';
+import React, {Component} from 'react';
 
-const App = () => (
-    <div>
-        <div className="app">
-            <img src={reactPumpkin} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-            <img src={webs} className="webs-r"/>
-            <img src={webs} className="webs-l"/>
-            <img src={spider} className="spider shake"/>
-        </div>
-    </div>
-);
+
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            currentQuestion:null,
+            answers=[],
+        }
+    }
+}
 
 export default App;
