ec80839d5a11ddfe3579127f73415bf13f17da49
Ryan Soto <rsoto003@ucr.edu>
2018-03-21 11:23:49 -0700

    random q is ready

--- src/helpers/question.js
+++ src/helpers/question.js
@@ -1,9 +1,7 @@
 
 
 export function genRandomQuestion(arr){
-    return Math.floor(Math.random() * arr.length);
+    const question = genRandomQuestion(questions);
+    return arr[question];
 }
 
-const question = genRandomQuestion(questions);
-
-const lol = Math.floor(Math.random()*questions.length)
